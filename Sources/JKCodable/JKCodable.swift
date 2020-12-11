3b36037f0790f24acdbb09edbd33faffe2bce684
tangjunjie <921969987@qq.com>
2020-12-11 18:13:28 +0800

    5.0.2

--- Sources/JKCodable/JKCodable.swift
+++ Sources/JKCodable/JKCodable.swift
@@ -21,72 +21,59 @@ public extension Decodable {
     
     typealias T = Self
     
-    static func fromJsonData(_ jsonData: Data) -> T? {
+    static func fromData(_ data: Data?) -> T? {
+        
+        guard let data = data else { return nil }
         
         let decoder = JSONDecoder()
-        if let obj = try? decoder.decode(T.self, from: jsonData) {
-            return obj
-        }
-        print("JKCodable.Decodable.fromJsonData: decoder.decode failure")
-        return nil
-    }
-    
-    
-    static func fromJsonDic(_ jsonDic: [String: Any]) -> T? {
+        var obj: T? = nil
         
-        if let data = try? JSONSerialization.data(withJSONObject: jsonDic, options: [.prettyPrinted]) {
-            return fromJsonData(data)
+        do {
+            obj = try decoder.decode(T.self, from: data)
+        } catch{
+            print("JKCodable.Decodable.fromJsonData: \n\(error)")
         }
-        print("JKCodable.Decodable.fromJsonDic: JSONSerialization.data failure")
-        return nil
-    }
-    
-    
-    static func fromJsonString(_ jsonString: String) -> T? {
         
-        if let data = jsonString.data(using: .utf8) {
-            return fromJsonData(data)
+        return obj
+    }
+    
+    
+    static func fromJson(_ json: Any?) -> T? {
+        
+        guard let json = json else { return nil }
+        
+        if let data = Data.fromJson(json) {
+            return fromData(data)
         }
-        print("JKCodable.Decodable.fromJsonString: jsonString.data failure")
         return nil
     }
+    
 }
 
 public extension Encodable {
     
-    typealias T = Self
-    
-    func toJsonString() -> String? {
+    func toJson() -> Any? {
         
-        if let data = toJsonData() {
-            return String(data: data, encoding: .utf8)
-        }
-        print("JKCodable.Encodable.toJsonString: String.data failure")
-        return nil
+        var result: Any? = nil
+        guard let data = toData() else { return result }
+        
+        result = data.toJson()
+        return result
     }
     
     
-    func toJsonDic() -> [String: Any]? {
-        
-        if let data = toJsonData() {
-            
-            if let dic = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
-                return dic as? [String: Any]
-            }
-            print("JKCodable.Encodable.toJsonDic: JSONSerialization.jsonObject failure")
-        }
-        return nil
-    }
-    
-    
-    func toJsonData() -> Data? {
+    func toData() -> Data? {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
-        if let data = try? encoder.encode(self) {
-            return data
+        var result: Data? = nil
+        
+        do {
+            result = try encoder.encode(self)
+        } catch {
+            print("JKCodable.Encodable.toJsonData: \n \(error)")
         }
-        print("JKCodable.Encodable.toJsonData: encoder.encode failure")
-        return nil
+        
+        return result
     }
     
 }
@@ -95,74 +82,81 @@ public extension Encodable {
 
 public extension Array where Element: Codable {
     
-    func toJsonString() -> String? {
-        
-        if let data = toJsonData() {
-            return String(data: data, encoding: .utf8)
-        }
-        return nil
+    
+    typealias T = Self
+    
+    static func fromJson(_ json: Any?) -> T? {
+        let data = Data.fromJson(json)
+        return fromData(data)
     }
     
-    
-    static func fromJsonString(_ jsonString: String) -> [Element]? {
+    static func fromData(_ data: Data?) -> T? {
         
-        if let data = jsonString.data(using: .utf8) {
-            return fromJsonData(data)
+        var result: T? = nil
+        guard let data = data else { return result }
+        
+        do {
+            result = try JSONSerialization.jsonObject(with: data, options: [.mutableContainers]) as? T
+        } catch {
+            print("JKCodable.Array.fromData: \n \(error)")
         }
-        print("JKCodable.Array.Codable.fromJsonString: jsonString.data failure")
-        return nil
+        return result
     }
     
-    
-    func toDicArray() -> [[String: Any]]? {
-        
-        if let data = toJsonData() {
-            if let dic = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
-                return dic as? [[String: Any]]
-            }
-            print("JKCodable.Array.Codable.toDicArray: JSONSerialization.jsonObject failure")
-        }
-        return nil
-    }
-    
-    static func fromDicArray(_ jsonDic: [String: Any]) -> [Element]? {
-        
-        if let data = try? JSONSerialization.data(withJSONObject: jsonDic, options: [.prettyPrinted]) {
-            return fromJsonData(data)
-        }
-        print("JKCodable.Array.Codable.fromDicArray: JSONSerialization.data failure")
-        return nil
-    }
-    
-    
-    
-    
-    func toJsonData() -> Data? {
+    func toJson() -> Any? {
         let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted
-        if let data = try? encoder.encode(self) {
-            return data
+        var result: Any? = nil
+        
+        do {
+            result = try encoder.encode(self)
+        } catch {
+            print("JKCodable.Array.toJson: \n \(error)")
         }
-        print("JKCodable.Array.Codable.toJsonData: encoder.encode failure")
-        return nil
+        return result
     }
     
-    static func fromJsonData(_ jsonData: Data) -> [Element]? {
+    func toData() -> Data? {
+        let json = toJson()
+        return Data.fromJson(json)
+    }
+    
+
+}
+
+
+
+extension Data {
+    
+    static func fromJson(_ json: Any?) -> Self? {
+    
+        var result: Data? = nil
+        guard let json = json else { return result }
         
-        let decoder = JSONDecoder()
-        if let obj = try? decoder.decode([Element].self, from: jsonData) {
-            return obj
+        do {
+            result = try JSONSerialization.data(withJSONObject: json, options: [.prettyPrinted])
+        } catch {
+            print("JKCodable.Data.fromJson: \n \(error)")
         }
-        print("JKCodable.Array.Codable.fromJsonData: decoder.decode failure")
-        return nil
+        return result
+    }
+    
+    func toJson() -> Any? {
+        var result: Any? = nil
+        do {
+            result = try JSONSerialization.jsonObject(with: self, options: [.mutableContainers])
+        } catch {
+            print("JKCodable.Data.toJson: \n \(error)")
+        }
+        return result
     }
     
 }
 
 
-
-
-
+struct Json: Codable {
+    
+    
+}
 
 
 
